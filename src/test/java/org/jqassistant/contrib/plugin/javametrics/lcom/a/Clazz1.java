14931333c2c992fcc68f1a2145600a21984e2d46
Jürgen Dufner <juergen@jdufner.de>
2019-05-13 21:32:57 +0200

    intermediate step

---
+++ src/test/java/org/jqassistant/contrib/plugin/javametrics/lcom/a/Clazz1.java
@@ -0,0 +1,19 @@
+package org.jqassistant.contrib.plugin.javametrics.lcom.a;
+
+final class Clazz1 {
+
+    private int field1;
+
+    Clazz1(int field1) {
+        this.field1 = field1;
+    }
+
+    public int getField1() {
+        return field1;
+    }
+
+    public void setField1(int field1) {
+        this.field1 = field1;
+    }
+
+}
